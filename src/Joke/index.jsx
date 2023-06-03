301488087d3f53c2523edd44abc5c596d5a5692f
Andrea Wimberská <awimberska@gmail.com>
2023-06-03 15:05:41 +0200

    Vytvořte komponentu Joke, která zobrazuje jeden vtip. Tato komponenta bude sídlet ve vlastní složce se všemi potřebnými CSS styly a obrázky. Komponenta Joke nechť má pět props: userAvatar, userName, text, likes, dislikes.

--- src/Joke/index.jsx
+++ src/Joke/index.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import './index.css';
 
-export const Jokes = () => {
-  const [upLikes, setUpLikes] = useState(0);
-  const [downLikes, setDownLikes] = useState(0);
+export const Jokes = ({ userAvatar, userName, text, likes, dislikes }) => {
+  const [upLikes, setUpLikes] = useState(likes);
+  const [downLikes, setDownLikes] = useState(dislikes);
 
   const handleUpClick = () => {
     setUpLikes(upLikes + 1);
@@ -17,18 +17,11 @@ export const Jokes = () => {
       <div className="joke">
         <div className="joke__body">
           <div className="joke__user">
-            <img
-              className="user-avatar"
-              src="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png"
-            />
-            <p className="user-name">Neroxx</p>
+            <img className="user-avatar" src={userAvatar} />
+            <p className="user-name">{userName}</p>
           </div>
 
-          <p className="joke__text">
-            The secret service isn't allowed to yell "Get down!" anymore when
-            the president is about to be attacked. Now they have to yell
-            "Donald, duck!"
-          </p>
+          <p className="joke__text">{text}</p>
         </div>
         <div className="joke__likes">
           <button
--- src/index.jsx
+++ src/index.jsx
@@ -6,9 +6,15 @@ import { Jokes } from './Joke';
 const App = () => {
   return (
     <>
-    <Jokes/>
+      <Jokes
+        userAvatar="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png"
+        userName="Neroxx"
+        text={`The secret service isn't allowed to yell "Get down!" anymore when the president is about to be attacked. Now they have to yell "Donald, duck!"`}
+        likes={0}
+        dislikes={0}
+      />
     </>
-  )
+  );
 };
 
 createRoot(document.querySelector('#app')).render(<App />);
